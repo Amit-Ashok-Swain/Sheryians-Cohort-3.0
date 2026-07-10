20e324e490917f9e0e8f5cfb3661e00b87c07580
Amit Swain <business.amitswain@gmail.com>
2026-07-10 15:03:36 +0530

    vite config updated

--- JavaScript/DOM/Projects/Productive-Dashboard/vite.config.js
+++ JavaScript/DOM/Projects/Productive-Dashboard/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: "/Sheryians-Cohort-3.0/JavaScript/DOM/Projects/Productive-Dashboard/dist/",
-  server: { port: 3000, open: true }
+  // Base should just be /<repo-name>/
+  base: "/Sheryians-Cohort-3.0/",
+  server: { 
+    port: 3000, 
+    open: true 
+  }
 });
-
-
